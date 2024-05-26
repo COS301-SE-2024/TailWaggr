8de551974bf9e83bccf22c0cacf5ae45b08d6b44
Scott-Bebington <133017388+Scott-Bebington@users.noreply.github.com>
2024-05-26 20:27:04 +0200

    Initial boilerplate code

---
+++ cos301_capstone/android/app/src/main/kotlin/com/example/cos301_capstone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cos301_capstone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ cos301_capstone/lib/main.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Argonauts - TailWaggr', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the COS 301 Capstone project',
+            ),
+            const Text(
+              'Argonauts - TailWaggr',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
