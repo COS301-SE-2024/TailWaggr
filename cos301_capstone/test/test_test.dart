c6a8a81c10225e47924349b77605bff3fc6ad665
Tim Tam <timothymdevilliers@gmail.com>
2024-06-02 19:37:01 +0200

    wrote a simple test to test coverage

--- cos301_capstone/test/test_test.dart
+++ cos301_capstone/test/test_test.dart
@@ -1,7 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
+import '.cos301_capstone/test_function.dart';
 
 void main() {
-    test('Demo test', () {
-        expect(1, 1);
+    test('Test add function', () {
+        // Arrange
+        var a = 5;
+        var b = 7;
+        var expected = 12;
+
+        // Act
+        var result = add(a, b); // Call the function you want to test
+
+        // Assert
+        expect(result, expected);
     });
 }
