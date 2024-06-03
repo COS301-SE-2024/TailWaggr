386f68af42f82c40a1117828d701e23e29c84909
Tim Tam <timothymdevilliers@gmail.com>
2024-06-03 13:26:05 +0200

    Did some more tests

--- cos301_capstone/test/location_test.dart
+++ cos301_capstone/test/location_test.dart
@@ -1,3 +1,5 @@
+import 'package:cos301_capstone/Location/Mobile_View.dart';
+import 'package:cos301_capstone/Location/Tablet_View.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cos301_capstone/Location/Desktop_View.dart';
@@ -35,6 +37,26 @@ void main() {
 
     // Verify that the GoogleMapController is set
     expect(mockController, isNotNull);
+
+    // Restore the original error handler
+    FlutterError.onError = oldOnError;
+  });
+  testWidgets('LocationMobile widget test', (WidgetTester tester) async {
+    // Ignore overflow errors
+    final oldOnError = FlutterError.onError;
+    FlutterError.onError = (FlutterErrorDetails details) {
+      if (!details.exceptionAsString().contains('A RenderFlex overflowed by')) {
+        oldOnError!(details);
+      }
+    };
+    // Build the LocationMobile widget.
+    await tester.pumpWidget(MaterialApp(home: LocationMobile()));
+
+    // Verify that the LocationMobile widget is displayed.
+    expect(find.byType(LocationMobile), findsOneWidget);
+
+    // Verify that the Placeholder widget is displayed.
+    expect(find.byType(Placeholder), findsOneWidget);
 
     // Restore the original error handler
     FlutterError.onError = oldOnError;
--- cos301_capstone/test/signup_test.dart
+++ cos301_capstone/test/signup_test.dart
@@ -1,4 +1,5 @@
 import 'package:cos301_capstone/Signup/Desktop_View.dart';
+import 'package:cos301_capstone/Signup/Mobile_View.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cos301_capstone/Signup/Signup.dart';
@@ -11,7 +12,7 @@ void main() {
   testWidgets('Desktop_Signup widget test', (WidgetTester tester) async {
     // Mock the FirebaseAuth
     final mockAuth = MockFirebaseAuth();
-
+    await tester.pumpWidget(MaterialApp(home: Signup()));
     // Build the Desktop_Signup widget.
     await tester.pumpWidget(MaterialApp(home: Desktop_Signup()));
 
@@ -21,5 +22,15 @@ void main() {
     // Verify that the TextFields and ElevatedButton are displayed.
     expect(find.byType(TextField), findsNWidgets(5));
     expect(find.byType(ElevatedButton), findsOneWidget);
+  });
+  testWidgets('Mobile_Signup widget test', (WidgetTester tester) async {
+    // Build the Mobile_Signup widget.
+    await tester.pumpWidget(MaterialApp(home: Mobile_Signup()));
+
+    // Verify that the Mobile_Signup widget is displayed.
+    expect(find.byType(Mobile_Signup), findsOneWidget);
+
+    // Verify that the "Sign up" text is displayed.
+    expect(find.text('Sign up'), findsOneWidget);
   });
 }
