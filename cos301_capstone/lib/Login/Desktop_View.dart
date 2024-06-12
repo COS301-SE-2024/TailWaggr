300fdd0e0517f688e85f6e8ca7d7853cc791a1da
Scott-Bebington <133017388+Scott-Bebington@users.noreply.github.com>
2024-06-12 10:26:23 +0200

    removed email print for login

--- cos301_capstone/lib/Login/Desktop_View.dart
+++ cos301_capstone/lib/Login/Desktop_View.dart
@@ -187,9 +187,6 @@ class _DesktopLoginState extends State<DesktopLogin> {
                                     LoginText = 'Logging in...';
                                   });
 
-                                  print("Signing in with Email and Password");
-                                  print("Email: ${signInEmailController.text}");
-
                                   await FirebaseAuth.instance.signInWithEmailAndPassword(
                                     email: signInEmailController.text,
                                     password: signInPasswordController.text,
