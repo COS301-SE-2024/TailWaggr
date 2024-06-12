d0385a0de76b612818c557a628abcea958fde098
GivenChauke <chaukemnashe123@gmail.com>
2024-06-12 22:38:06 +0200

    Add sign up with google and add user details to the db

--- cos301_capstone/lib/services/auth/auth.dart
+++ cos301_capstone/lib/services/auth/auth.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<User?> signIn(String email, String password) async {
     try {
@@ -78,5 +80,55 @@ Future<String?> getUserByAuthId(String authId) async {
     return null;
   }
 }
+  Future<User?> signInWithGoogle() async {
+    try {
+      // Trigger the Google authentication flow
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        // User canceled the sign-in
+        return null;
+      }
 
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      // Create a new credential
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Sign in to Firebase with the Google credential
+      UserCredential result = await _auth.signInWithCredential(credential);
+      User? user = result.user;
+
+      if (user != null) {
+        // Check if the user is new and create a Firestore document if so
+        DocumentSnapshot userDoc = await _db.collection('users').doc(user.uid).get();
+        if (!userDoc.exists) {
+          // If the user does not exist in Firestore, create a new user document
+          await _db.collection('users').doc(user.uid).set({
+            'authId': user.uid,
+            'name': user.displayName,
+            'surname': '', // Adjust as needed, Google sign-in may not provide surname
+            'email': user.email,
+            'typeUser': 'pet_owner'
+          });
+
+          // Create a profile document
+          await _db.collection('profile').doc(user.uid).set({
+            'Bio': '',
+            'DarkMode': false,
+            'ProfileImg': '',
+            'UserId': _db.collection('users').doc(user.uid)
+          });
+        }
+      }
+
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
