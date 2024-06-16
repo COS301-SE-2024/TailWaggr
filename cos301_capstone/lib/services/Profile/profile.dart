8c3a4c4d508355886940509993b0bb33304dda13
GivenChauke <chaukemnashe123@gmail.com>
2024-06-16 09:03:35 +0200

    profile and auth  functions bug fixes

--- cos301_capstone/lib/services/Profile/profile.dart
+++ cos301_capstone/lib/services/Profile/profile.dart
@@ -8,7 +8,7 @@ class ProfileService {
 
   Future<Map<String, dynamic>?> getUserProfile(String userId) async {
     try {
-      DocumentSnapshot doc = await _db.collection('profiles').doc(userId).get();
+      DocumentSnapshot doc = await _db.collection('profile').doc(userId).get();
       if (doc.exists) {
         return doc.data() as Map<String, dynamic>?;
       } else {
@@ -22,7 +22,7 @@ class ProfileService {
   }
   Future<Map<String, dynamic>?> getPetProfile(String petId) async {
       try {
-        DocumentSnapshot doc = await _db.collection('pets').doc(petId).get();
+        DocumentSnapshot doc = await _db.collection('petProfile').doc(petId).get();
         if (doc.exists) {
           return doc.data() as Map<String, dynamic>?;
         } else {
@@ -36,7 +36,7 @@ class ProfileService {
     }
   Future<void> updateProfile(String userId, Map<String, dynamic> updatedData) async {
     try {
-      await _db.collection('profiles').doc(userId).update(updatedData);
+      await _db.collection('profile').doc(userId).update(updatedData);
     } catch (e) {
       print("Error updating profile: $e");
     }
@@ -58,11 +58,26 @@ class ProfileService {
       return null;
     }
   }
+    Future<String?> updatePetProfileImage(String ownerId, File file) async {
+    try {
+      Reference ref = _storage.ref().child('profile_images/$ownerId');
+      UploadTask uploadTask = ref.putFile(file);
 
+      TaskSnapshot snapshot = await uploadTask;
+      String downloadUrl = await snapshot.ref.getDownloadURL();
+
+      await updatePet(ownerId, {'profileImg': downloadUrl});
+
+      return downloadUrl;
+    } catch (e) {
+      print("Error updating profile image: $e");
+      return null;
+    }
+  }
   Future<List<DocumentReference>> getUserLikes(String userId) async {
     try {
       QuerySnapshot snapshot = await _db.collection('likes')
-          .where('userId', isEqualTo: _db.collection('users').doc(userId))
+          .where('UserId', isEqualTo: _db.collection('users').doc(userId))
           .get();
       return snapshot.docs.map((doc) => doc.reference).toList();
     } catch (e) {
@@ -74,7 +89,7 @@ class ProfileService {
   Future<List<DocumentReference>> getUserSaves(String userId) async {
     try {
       QuerySnapshot snapshot = await _db.collection('saves')
-          .where('userId', isEqualTo: _db.collection('users').doc(userId))
+          .where('UserId', isEqualTo: _db.collection('users').doc(userId))
           .get();
       return snapshot.docs.map((doc) => doc.reference).toList();
     } catch (e) {
@@ -98,7 +113,7 @@ class ProfileService {
   Future<List<DocumentReference>> getUserPosts(String userId) async {
     try {
       QuerySnapshot snapshot = await _db.collection('posts')
-          .where('userId', isEqualTo: _db.collection('users').doc(userId))
+          .where('UserId', isEqualTo: _db.collection('users').doc(userId))
           .get();
       return snapshot.docs.map((doc) => doc.reference).toList();
     } catch (e) {
@@ -109,7 +124,7 @@ class ProfileService {
 
   Future<void> addPet(String ownerId, Map<String, dynamic> petData) async {
     try {
-      DocumentReference petRef = await _db.collection('pets').add(petData);
+      DocumentReference petRef = await _db.collection('petProfile').add(petData);
       await updateProfile(ownerId, {'petIds': FieldValue.arrayUnion([petRef])});
     } catch (e) {
       print("Error adding pet: $e");
@@ -118,7 +133,7 @@ class ProfileService {
 
   Future<void> updatePet(String petId, Map<String, dynamic> updatedData) async {
     try {
-      await _db.collection('pets').doc(petId).update(updatedData);
+      await _db.collection('petProfile').doc(petId).update(updatedData);
     } catch (e) {
       print("Error updating pet details: $e");
     }
