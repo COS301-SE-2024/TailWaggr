971408ae6ba1f5d3399075d39d59e4890d171ddc
Scott-Bebington <133017388+Scott-Bebington@users.noreply.github.com>
2024-06-21 21:44:35 +0200

    Created a mock set of posts for the desktop view

--- cos301_capstone/lib/Homepage/Desktop_View.dart
+++ cos301_capstone/lib/Homepage/Desktop_View.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'dart:math';
+
 import 'package:cos301_capstone/Global_Variables.dart';
 import 'package:cos301_capstone/Homepage/Homepage.dart';
 import 'package:cos301_capstone/Navbar/Desktop_View.dart';
@@ -49,6 +51,163 @@ class _PostContainerState extends State<PostContainer> {
         color: themeSettings.cardColor,
         borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Post(),
+            Divider(),
+            Post(),
+            Divider(),
+            Post(),
+            Divider(),
+            Post(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Post extends StatefulWidget {
+  const Post({super.key});
+
+  @override
+  State<Post> createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width * 0.5 - 290,
+          // color: const Color.fromARGB(179, 0, 0, 0),
+          padding: EdgeInsets.only(right: 20),
+          height: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(profileDetails.profilePicture),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        profileDetails.name,
+                        style: TextStyle(
+                          color: themeSettings.textColor,
+                        ),
+                      ),
+                      Text(
+                        "Posted on ${DateTime.now().toString().split(" ")[0]}",
+                        style: TextStyle(
+                          color: themeSettings.textColor.withOpacity(0.7),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                style: TextStyle(
+                  color: themeSettings.textColor,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 4,
+              ),
+              Spacer(),
+              if (profileDetails.pets.isNotEmpty) ...[
+                Text(
+                  "Pets included in this post: ",
+                  style: TextStyle(
+                    color: themeSettings.textColor.withOpacity(0.7),
+                  ),
+                ),
+                SizedBox(height: 10),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (var pet in profileDetails.pets) ...[
+                        Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Column(
+                            children: [
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundImage: NetworkImage(pet["profilePicture"]),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                pet["name"],
+                                style: TextStyle(color: themeSettings.textColor),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+              ],
+              Spacer(),
+              Row(
+                children: [
+                  Tooltip(
+                    message: "Like",
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite_border,
+                        color: Colors.red.withOpacity(0.7),
+                      ),
+                    ),
+                  ),
+                  Text("0", style: TextStyle(color: themeSettings.textColor.withOpacity(0.7))),
+                  Spacer(),
+                  Tooltip(
+                    message: "Comment",
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.comment,
+                        color: Colors.blue.withOpacity(0.7),
+                      ),
+                    ),
+                  ),
+                  Text("0", style: TextStyle(color: themeSettings.textColor.withOpacity(0.7))),
+                  Spacer(),
+                  Tooltip(
+                    message: "Views",
+                    child: Icon(
+                      Icons.bar_chart,
+                      color: Colors.green.withOpacity(0.7),
+                    ),
+                  ),
+                  Text("0", style: TextStyle(color: themeSettings.textColor.withOpacity(0.7))),
+                ],
+              ),
+            ],
+          ),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.network(
+            profileDetails.pets[Random().nextInt(9)]["profilePicture"],
+            width: MediaQuery.of(context).size.width * 0.24775,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
     );
   }
 }
