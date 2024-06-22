5f8b1fce6d0978c6877ec30630641404371abfb7
Tim Tam <timothymdevilliers@gmail.com>
2024-06-22 14:34:53 +0200

    Changed the DB and added a getUserPets

--- cos301_capstone/lib/services/HomePage/home_page_service.dart
+++ cos301_capstone/lib/services/HomePage/home_page_service.dart
@@ -92,5 +92,20 @@ class HomePageService {
       return []; // Return an empty list if an error occurs
     }
   }
+  Future<List<Map<String, dynamic>>> getUserPets(String userId) async {
+    try {
+      // Access the user's "pets" subcollection
+      final querySnapshot = await _db.collection('users').doc(userId).collection('pets').get();
+
+      // Convert each document to a map and add it to a list
+      final pets = querySnapshot.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+
+      print("Pets fetched successfully.");
+      return pets; // Return the list of pets
+    } catch (e) {
+      print("Error fetching pets: $e");
+      return []; // Return an empty list if an error occurs
+    }
+  }
 
 }
