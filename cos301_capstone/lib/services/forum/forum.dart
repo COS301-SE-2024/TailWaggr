c14ba48c287e228f627fad8cc5d0bf92c09c0151
GivenChauke <chaukemnashe123@gmail.com>
2024-06-23 18:52:57 +0200

    bug fixes in forums

--- cos301_capstone/lib/services/forum/forum.dart
+++ cos301_capstone/lib/services/forum/forum.dart
@@ -7,27 +7,26 @@ class ForumServices {
 
   /// Creates a new forum in the database.
   Future<DocumentReference<Object?>> createForum({
-    String? name,//forum name
-    String? userId//userId of forum owner
+    String? name, // forum name
+    String? userId // userId of forum owner
   }) async {
     try {
-
       // Get imgUrl
       DocumentSnapshot postDoc = await _db.collection('profile').doc(userId).get();
       if (!postDoc.exists) throw Exception("Post not found");
       Map<String, dynamic> postData = postDoc.data() as Map<String, dynamic>;
-      String imgUrl = postData['ImgUrl']??'';
+      String imgUrl = postData['ImgUrl'] ?? '';
 
-      //get Time
+      // Get Time
       DateTime now = DateTime.now();
-      DocumentReference Ref = await _db.collection('forum').add({
+      DocumentReference ref = await _db.collection('forum').add({
         'CreatedAt': now,
         'ImgUrl': imgUrl,
         'Name': name,
-        'UserId': _db.doc("users/$userId")
+        'UserId': userId
       });
       print('Forum created successfully.');
-      return Ref;
+      return ref;
     } catch (e) {
       print('Error creating forum: $e');
       throw Exception('Failed to create forum.');
@@ -41,9 +40,7 @@ class ForumServices {
 
       List<Map<String, dynamic>> forums = [];
       for (DocumentSnapshot doc in forumsSnapshot.docs) {
-        print(forumsSnapshot);
         Map<String, dynamic>? forumData = doc.data() as Map<String, dynamic>?;
-        print(forumData);
         if (forumData != null) {
           forums.add({
             'forumId': doc.id,
@@ -51,7 +48,7 @@ class ForumServices {
           });
         }
       }
-
+      print(forums);
       return forums.isNotEmpty ? forums : null;
     } catch (e) {
       print('Error fetching forums: $e');
@@ -59,121 +56,114 @@ class ForumServices {
     }
   }
 
-  /// Retrieves posts associated with a forum including likes and replies.
-  Future<List<Map<String, dynamic>>?> getPosts(String forumId) async {
-  try {
-    DocumentReference forumRef = _db.doc('forum/$forumId');
-    QuerySnapshot postsSnapshot = await _db
-        .collection('posts')
-        .where('ForumId', isEqualTo: forumRef)
-        .get();
-
-    List<Map<String, dynamic>> posts = [];
-    for (DocumentSnapshot postDoc in postsSnapshot.docs) {
-      Map<String, dynamic>? postData = postDoc.data() as Map<String, dynamic>?;
-
-      if (postData != null) {
-        // Fetch user profile details for the post creator
-        DocumentReference userRef = postData['UserId'] as DocumentReference;
-        DocumentSnapshot userProfileSnapshot = await userRef.get();
-        Map<String, dynamic>? userProfile = userProfileSnapshot.data() as Map<String, dynamic>?;
-
-        // Fetch likes for the post
-        QuerySnapshot likesSnapshot = await postDoc.reference.collection('likes').get();
-        List<Map<String, dynamic>> likes = [];
-        for (DocumentSnapshot likeDoc in likesSnapshot.docs) {
-          Map<String, dynamic>? likeData = likeDoc.data() as Map<String, dynamic>?;
-
-          if (likeData != null) {
-            likes.add({
-              'likeId': likeDoc.id,
-              ...likeData,
-            });
-          }
-        }
-
-        // Fetch replies for the post
-        QuerySnapshot repliesSnapshot = await postDoc.reference.collection('replies').get();
-        List<Map<String, dynamic>> replies = [];
-        for (DocumentSnapshot replyDoc in repliesSnapshot.docs) {
-          Map<String, dynamic>? replyData = replyDoc.data() as Map<String, dynamic>?;
-          if (replyData != null) {
-            replies.add({
-              'replyId': replyDoc.id,
-              ...replyData,
-            });
-          }
-        }
-
-        posts.add({
-          'post': postData,
-          'creator': userProfile,
-          'likes': likes,
-          'replies': replies,
-        });
-      }
-    }
-
-    return posts.isNotEmpty ? posts : null;
-  } catch (e) {
-    print('Error fetching posts: $e');
-    return null;
-  }
-}
-
-  /// Creates a new post in Firestore.
-  Future<DocumentReference?> createPost(String forumId, String userId, String content) async {
+  /// Retrieves messages associated with a forum including likes and replies.
+  Future<List<Map<String, dynamic>>?> getMessages(String forumId) async {
     try {
-      // Create a new post document
-      DocumentReference postRef = await _db.collection('posts').add({
-        'ForumId': _db.doc('forum/$forumId'),
-        'UserId': _db.doc('users/$userId'),
+      QuerySnapshot messagesSnapshot = await _db
+          .collection('forum')
+          .doc(forumId)
+          .collection('messages')
+          .get();
+
+      List<Map<String, dynamic>> messages = [];
+      for (DocumentSnapshot messageDoc in messagesSnapshot.docs) {
+        Map<String, dynamic>? messageData = messageDoc.data() as Map<String, dynamic>?;
+
+        if (messageData != null) {
+          // Fetch likes for the message
+          QuerySnapshot likesSnapshot = await messageDoc.reference.collection('likes').get();
+          List<Map<String, dynamic>> likes = [];
+          for (DocumentSnapshot likeDoc in likesSnapshot.docs) {
+            Map<String, dynamic>? likeData = likeDoc.data() as Map<String, dynamic>?;
+            if (likeData != null) {
+              likes.add({
+                'likeId': likeDoc.id,
+                ...likeData,
+              });
+            }
+          }
+
+          // Fetch replies for the message
+          QuerySnapshot repliesSnapshot = await messageDoc.reference.collection('replies').get();
+          List<Map<String, dynamic>> replies = [];
+          for (DocumentSnapshot replyDoc in repliesSnapshot.docs) {
+            Map<String, dynamic>? replyData = replyDoc.data() as Map<String, dynamic>?;
+            if (replyData != null) {
+              replies.add({
+                'replyId': replyDoc.id,
+                ...replyData,
+              });
+            }
+          }
+
+          messages.add({
+            'messageId': messageDoc.id, // Corrected here to add messageId
+            'message': messageData,
+            'likes': likes,
+            'replies': replies,
+          });
+        }
+      }
+      print(messages);
+      return messages.isNotEmpty ? messages : null;
+    } catch (e) {
+      print('Error fetching messages: $e');
+      return null;
+    }
+  }
+  /// Creates a new message in Firestore.
+  Future<DocumentReference?> createMessage(String forumId, String userId, String content) async {
+    try {
+      // Create a new message document
+      DocumentReference messageRef = await _db.collection('forum').doc(forumId).collection('messages').add({
+        'UserId': userId,
         'Content': content,
         'CreatedAt': Timestamp.now(),
       });
 
-      return postRef;
+      return messageRef;
     } catch (e) {
-      print('Error creating post: $e');
+      print('Error creating message: $e');
       return null;
     }
   }
-  /// Likes a post in Firestore.
-Future<DocumentReference?> likePost(String postId, String userId) async {
-  try {
-    // Add a new like document under the post
-    DocumentReference likeRef = await _db.collection('posts').doc(postId).collection('likes').add({
-      'UserId': _db.doc('users/$userId'),
-      'CreatedAt': Timestamp.now(),
-    });
-    //create Like notification
-    notif.createLikeNotification(postId,userId);
 
-    return likeRef;
-  } catch (e) {
-    print('Error liking post: $e');
-    return null;
+  /// Likes a message in Firestore.
+  Future<DocumentReference?> likeMessage(String forumId, String messageId, String userId) async {
+    try {
+      // Add a new like document under the message
+      DocumentReference likeRef = await _db.collection('forum').doc(forumId).collection('messages').doc(messageId).collection('likes').add({
+        'UserId': userId,
+        'CreatedAt': Timestamp.now(),
+      });
+
+      // Create Like notification
+      notif.createLikeNotification(messageId, userId);
+
+      return likeRef;
+    } catch (e) {
+      print('Error liking message: $e');
+      return null;
+    }
+  }
+
+  /// Replies to a message in Firestore.
+  Future<DocumentReference?> replyToMessage(String forumId, String messageId, String userId, String content) async {
+    try {
+      // Add a new reply document under the message
+      DocumentReference replyRef = await _db.collection('forum').doc(forumId).collection('messages').doc(messageId).collection('replies').add({
+        'UserId': userId,
+        'Content': content,
+        'CreatedAt': Timestamp.now(),
+      });
+
+      // Create reply notification
+      notif.createReplyNotification(messageId, userId);
+
+      return replyRef;
+    } catch (e) {
+      print('Error replying to message: $e');
+      return null;
+    }
   }
 }
-/// Replies to a post in Firestore.
-Future<DocumentReference?> replyToPost(String postId, String userId, String content) async {
-  try {
-    // Add a new reply document under the post
-    DocumentReference replyRef = await _db.collection('posts').doc(postId).collection('replies').add({
-      'UserId': _db.doc('users/$userId'),
-      'Content': content,
-      'CreatedAt': Timestamp.now(),
-    });
-    //create reply notification
-    notif.createReplyNotification(postId, userId);
-
-    return replyRef;
-  } catch (e) {
-    print('Error replying to post: $e');
-    return null;
-  }
-}
-
-}
-
-
