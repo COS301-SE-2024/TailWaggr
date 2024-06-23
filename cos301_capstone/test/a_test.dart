130dfb16703216e0c9a69b6a276fa6b87e4a4d99
NicholasHarvey <u22522116@tuks.co.za>
2024-06-24 01:19:39 +0200

    Update a_test.dart
    clarifying wording and first step of other test integration

--- cos301_capstone/test/a_test.dart
+++ cos301_capstone/test/a_test.dart
@@ -9,11 +9,28 @@ import 'package:cos301_capstone/services/profile/profile.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('integration tests', () {
-    testWidgets('help',
+  group('Integration tests', () {
+    testWidgets('Check that user profile data is accurate',
         (tester) async {
-      // User data
-      Future<Map<String, dynamic>?> pets = ProfileService().getUserProfile("QF5gHocYeGRNbsFmPE3RjUZIId82");
+          // User data
+          Future<Map<String, dynamic>?> profile = ProfileService().getUserProfile("QF5gHocYeGRNbsFmPE3RjUZIId82");
+    });
+    testWidgets('Check that user pets are accurate',
+        (tester) async {
+          // Pet data
+          Future<List<Map<String, dynamic>?>> pets = GeneralService().getUserPets("QF5gHocYeGRNbsFmPE3RjUZIId82");
+    });
+    testWidgets('Check that updating user profile works',
+        (tester) async {
+          // User data
+          Future<Map<String, dynamic>?> profile = ProfileService().getUserProfile("QF5gHocYeGRNbsFmPE3RjUZIId82");
+          // Update user data with a new string manually, then compare profile maps
+    });
+     testWidgets('Check that updating user pets works',
+        (tester) async {
+          // Pet data
+          Future<List<Map<String, dynamic>?>> pets = GeneralService().getUserPets("QF5gHocYeGRNbsFmPE3RjUZIId82");
+          // Update pet data with a new string manually, then compare profile maps
     });
   });
 }
