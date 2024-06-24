8f980612ebd8fd780e9a2ba4ce4c7c446a91db28
GivenChauke <chaukemnashe123@gmail.com>
2024-06-24 11:36:48 +0200

    display multiple user  usernames

--- cos301_capstone/lib/Forums/Desktop_View.dart
+++ cos301_capstone/lib/Forums/Desktop_View.dart
@@ -1,5 +1,3 @@
-
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
 import 'package:cos301_capstone/Global_Variables.dart';
 import 'package:cos301_capstone/Navbar/Desktop_View.dart';
